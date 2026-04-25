bcecb80065f923e29eab3d00db78bb8b32d09a07
pavan-dhake <pavan.dhake@finnovate.in>
2026-04-25 11:40:40 +0530

    u

--- data.js
+++ data.js
@@ -1,5 +1,9 @@
 window.SIGNATURES = [
   {
+    "name": "Humaid Khot",
+    "url": "https://pranobt.github.io/emailsign/HumaidK.html"
+  },
+  {
     "name": "Anuj Rane",
     "url": "https://pranobt.github.io/emailsign/AnujR.html"
   },
